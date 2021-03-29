fd65a887c0ef3cb0f2e0393f1a1491b58c2b0030
Monty <montgomery.okdaily@gmail.com>
2021-03-28 21:31:44 -0700

    Remove serial prints for requesting temperatures

--- ds18b20-temp-sensor.ino
+++ ds18b20-temp-sensor.ino
@@ -25,11 +25,12 @@ void loop(void)
 { 
   // call sensors.requestTemperatures() to issue a global temperature 
   // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
+  // Serial.print("Requesting temperatures...");
   sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
+  // Serial.println("DONE");
   
   Serial.print("Temperature for Device 1 is: ");
   Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
   
+  Serial.println("");
 }
